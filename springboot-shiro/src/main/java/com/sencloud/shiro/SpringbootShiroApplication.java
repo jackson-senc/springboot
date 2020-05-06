15a1b56d280789eb44ad2f676b5640cb74f21d5c
Jason <18860480588@163.com>
2020-05-06 14:13:03 +0800

    对代码进行修改

--- springboot-shiro/src/main/java/com/sencloud/shiro/SpringbootShiroApplication.java
+++ springboot-shiro/src/main/java/com/sencloud/shiro/SpringbootShiroApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootShiroApplication {
 
     public static void main(String[] args) {
+“dsdsdsd”
         SpringApplication.run(SpringbootShiroApplication.class, args);
     }
 
